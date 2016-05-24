1558266a0cf77419d3651c119120732a138145a3
Stefan Merkel <merkel.s.ulm@gmail.com>
2016-05-24 14:27:41 +0200

    Update

---
+++ Mvc4CSharpRazorFx45Intranet/Mvc4CSharpRazorFx45Intranet/Models/IdentityModels.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using AspNet.Identity.MySQL;
+
+namespace Mvc4CSharpRazorFx45Intranet.Models
+{
+    // Sie können Profildaten für den Benutzer durch Hinzufügen weiterer Eigenschaften zur ApplicationUser-Klasse hinzufügen. Weitere Informationen finden Sie unter "http://go.microsoft.com/fwlink/?LinkID=317594".
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Beachten Sie, dass der "authenticationType" mit dem in "CookieAuthenticationOptions.AuthenticationType" definierten Typ übereinstimmen muss.
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Benutzerdefinierte Benutzeransprüche hier hinzufügen
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : MySQLDatabase
+    {
+        public ApplicationDbContext(string connectionName)
+            : base(connectionName)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext("DefaultConnection");
+        }
+    }
+}
